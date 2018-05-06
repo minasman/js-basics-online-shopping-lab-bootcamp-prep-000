fbf1832ca3e26114060ac7376a23dc60b7ab8880
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 21:22:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,23 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var inCart = "In your cart, you have ";
   if (cart.length ===1 ){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}.`;
   }
   
-  if (cart.length ===1 ){
+  if (cart.length ===2 ){
     return `In your cart, you have ${cart[0].itemName} at $${cart[0].itemPrice}, and ${cart[1].itemName} at $${cart[1].itemPrice}.`;
   }
+  
+  if (cart.length > 2){
+    var entries = 0;
+    do {
+      inCart += `${cart[entries].itemName} at $${cart[entries].itemPrice}, `;
+      entries++;
+    } while (entries < cart.length - 2);
+  }
+  return inCart+=`${cart[cart.length - 2].itemName} at $${cart[cart.length - 2].itemPrice}, and ${cart[cart.length - 1].itemName} at $${cart[cart.length -1].itemPrice}.`;
   
 }
 
