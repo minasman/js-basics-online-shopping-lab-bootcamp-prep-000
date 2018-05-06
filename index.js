d4fbb1e2c37702d6b4cd0011bb64e0cf7d3f603f
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 21:34:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -54,10 +54,11 @@ function removeFromCart(item) {
   // write your code here
   for (var i=0; i<cart.length; i++){
     if(cart[i].itemName === item){
-      exists = true;
-      cart.splice()
+      cart.splice(i, 1);
+      return cart;
     }
   }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
