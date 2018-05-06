7705d5b676087b8fbdf026ffad4a4d4a48e740d9
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 21:36:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -63,4 +63,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
+  if(!cardNumber){
+    return "Sorry we don't have a credit card on file for you.";
+  }
+  
 }
