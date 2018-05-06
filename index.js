edbb47a1eeac959a862712652ecd4eb96a62ed58
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 21:32:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  for (var i=0; i<cart.length; i++){
+    if(cart[i].itemName === item){
+      exists = true;
+      cart.splice()
+    }
+  }
 }
 
 function placeOrder(cardNumber) {
